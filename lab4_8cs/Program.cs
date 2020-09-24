1e2d98b7679616515217c4751cd38619e54d2cd4
tsyduev.b.1@gmail.com <tsyduev.b.1@gmail.com>
2020-09-25 00:46:10 +0500

    Добавьте файлы проекта.

---
+++ lab4_8cs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net;           
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab4_8cs
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var parser = new Parser("https://www.susu.ru/ru/structure",100,10000);
+            parser.Finded += Console.WriteLine;
+            await parser.Analysis();
+            Console.WriteLine("asdas");
+        }
+        enum DataType
+        {
+            Number,Email
+        }
+        static void WriteCSV(string site, string data, DataType type)
+        {
+            
+        }
+    }
+}
